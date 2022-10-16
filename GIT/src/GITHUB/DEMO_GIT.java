5d3eba9c8876b9d5c87f2755abc11aa76c3980f7
MeghnaPatil20 <115784415+MeghnaPatil20@users.noreply.github.com>
2022-10-16 11:52:59 +0530

    Update DEMO_GIT.java

--- GIT/src/GITHUB/DEMO_GIT.java
+++ GIT/src/GITHUB/DEMO_GIT.java
@@ -5,9 +5,9 @@ public class DEMO_GIT
 	public static void main(String[] args) {
 		System.out.println("HELLO GIT");
 	}
-		public void m1()
-		{
-		System.out.println("HELLO GIT");	
+		public void meghna()
+	
+		System.out.println("I am MEGHNA");	
 	}
 
 }
