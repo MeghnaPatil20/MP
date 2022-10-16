cfd34c987aa6aa6fd8c9a964bd756b366c904c36
Admin <Admin@DESKTOP-93NBKK8>
2022-10-16 12:47:10 +0530

    By Meghna

--- GIT/src/GITHUB/DEMO_GIT.java
+++ GIT/src/GITHUB/DEMO_GIT.java
@@ -2,8 +2,10 @@ package GITHUB;
 
 public class DEMO_GIT
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("HELLO GIT");
+	}
 		public void m1()
 		{
 		System.out.println("HELLO GIT");	
