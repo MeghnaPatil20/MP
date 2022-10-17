1ab7f636db657be7d6600fed24a5a199cf3c3ae0
Admin <Admin@DESKTOP-93NBKK8>
2022-10-17 14:45:41 +0530

    trfctf

---
+++ git2/src/git3/v.java
@@ -0,0 +1,32 @@
+package git3;
+
+public class v
+{
+	
+
+	
+	
+		public static void main(String[] args) 
+		{
+			System.out.println("HELLO GIT");
+		}
+		
+			public void m1()
+			{
+			System.out.println("HELLO GIT");	
+			}
+
+			public void meghna()
+			{
+		System.out.println("MMMMM");
+				System.out.println("I am MEGHNA");	
+	 
+		}
+		public void pp()
+		{
+			System.out.println("PPPPPP");
+		}
+
+	}
+
+
